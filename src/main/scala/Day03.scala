97f1f698962a21a044e2a5689222fe00340eb7ea
Fred Fettinger <fettinge@gmail.com>
2020-12-03 01:22:48 -0600

    Day03 Solution

--- src/main/scala/Day03.scala
+++ src/main/scala/Day03.scala
@@ -1,11 +1,20 @@
 package com.fivebytestudios.wildfreddy
 
 object Day03 {
-  def part1(input: List[String]): Long = {
-    0
+  private def countTrees(input: List[String], slope: (Int, Int)): Long = {
+    val (dx, dy) = slope
+    (0 until input.length by dy)
+      .map(input)
+      .zip(0 to Int.MaxValue by dx)
+      .map{case (row, x) => row(x % row.length)}
+      .count(_ == '#')
   }
 
-  def part2(input: List[String]): Long = {
-    0
-  }
+  def part1(input: List[String]): Long =
+    countTrees(input, (3, 1))
+
+  def part2(input: List[String]): Long =
+    Seq((1, 1), (3, 1), (5, 1), (7, 1), (1, 2))
+      .map(countTrees(input, _))
+      .product
 }
