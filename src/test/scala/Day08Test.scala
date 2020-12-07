af24dce0c182037016a7e6ce887ed2d3fe96f442
Fred Fettinger <fettinge@gmail.com>
2020-12-07 00:04:27 -0600

    Day08 Template

---
+++ src/test/scala/Day08Test.scala
@@ -0,0 +1,32 @@
+package com.fivebytestudios.wildfreddy
+
+import helpers.ResourceHelpers
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class Day08Test extends AnyWordSpec with Matchers with ResourceHelpers {
+  override val resourcePath = "day08"
+  private val exampleData = getResourceLines("example")
+  private val inputData = getResourceLines("input")
+
+  "part1" should {
+    "pass example" in {
+      Day08.part1(exampleData) shouldBe 5
+    }
+
+    "produce result" in {
+      Day08.part1(inputData) shouldBe 0
+    }
+  }
+
+  "part2" should {
+    "pass example" in {
+      Day08.part2(exampleData) shouldBe 8
+    }
+
+    "produce result" in {
+      Day08.part2(inputData) shouldBe 0
+    }
+  }
+}
