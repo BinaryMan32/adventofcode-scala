bb3d2b80e4e7b9f74d978cf92db3e5ef66df7a41
Fred Fettinger <fettinge@gmail.com>
2020-12-08 01:39:24 -0600

    Day09 Template

---
+++ src/main/scala/Day09.scala
@@ -0,0 +1,11 @@
+package com.fivebytestudios.wildfreddy
+
+object Day09 {
+  def part1(input: List[String], preambleLength: Int): Option[Long] = {
+    None
+  }
+
+  def part2(input: List[String], preambleLength: Int): Option[Long] = {
+    None
+  }
+}
---
+++ src/test/scala/Day09Test.scala
@@ -0,0 +1,32 @@
+package com.fivebytestudios.wildfreddy
+
+import helpers.ResourceHelpers
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class Day09Test extends AnyWordSpec with Matchers with ResourceHelpers {
+  override val resourcePath = "day09"
+  private val exampleData = getResourceLines("example")
+  private val inputData = getResourceLines("input")
+
+  "part1" should {
+    "pass example" in {
+      Day09.part1(exampleData, preambleLength = 5) shouldBe Some(127)
+    }
+
+    "produce result" in {
+      Day09.part1(inputData, preambleLength = 25) shouldBe None
+    }
+  }
+
+  "part2" should {
+    "pass example" in {
+      Day09.part2(exampleData, preambleLength = 5) shouldBe Some(62)
+    }
+
+    "produce result" in {
+      Day09.part2(inputData, preambleLength = 25) shouldBe None
+    }
+  }
+}
