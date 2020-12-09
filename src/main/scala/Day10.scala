5c686b435e40a3ef6cef621a2946b48489dbe707
Fred Fettinger <fettinge@gmail.com>
2020-12-09 01:19:48 -0600

    Day10 Template

---
+++ src/main/scala/Day10.scala
@@ -0,0 +1,11 @@
+package com.fivebytestudios.wildfreddy
+
+object Day10 {
+  def part1(input: List[String]): Map[Int, Int] = {
+    Map.empty
+  }
+
+  def part2(input: List[String]): Long = {
+    0
+  }
+}
---
+++ src/test/scala/Day10Test.scala
@@ -0,0 +1,47 @@
+package com.fivebytestudios.wildfreddy
+
+import helpers.ResourceHelpers
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class Day10Test extends AnyWordSpec with Matchers with ResourceHelpers {
+  override val resourcePath = "day10"
+  private val exampleData = getResourceLines("example")
+  private val inputData = getResourceLines("input")
+
+  "part1" should {
+    "pass example" in {
+      Day10.part1(exampleData) should contain theSameElementsAs Map(
+        1 -> 7,
+        3 -> 5
+      )
+    }
+
+    "pass example2" in {
+      Day10.part1(getResourceLines("example2")) should contain theSameElementsAs Map(
+        1 -> 22,
+        3 -> 10
+      )
+    }
+
+    "produce result" in {
+      val jolts = Day10.part1(inputData)
+      jolts(1) * jolts(3) shouldBe 0
+    }
+  }
+
+  "part2" should {
+    "pass example" in {
+      Day10.part2(exampleData) shouldBe 8
+    }
+
+    "pass example2" in {
+      Day10.part2(getResourceLines("example2")) shouldBe 19208
+    }
+
+    "produce result" in {
+      Day10.part2(inputData) shouldBe 0
+    }
+  }
+}
