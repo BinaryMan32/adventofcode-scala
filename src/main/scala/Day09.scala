ceaa5b4f6761100e2ff92c0e3f73a067cf1994e5
Fred Fettinger <fettinge@gmail.com>
2020-12-09 01:19:19 -0600

    Day09 more efficient solution
    The initial version repeated a lot of work, since there's only one answer
    starting at each position in the input, and it would try that same position
    many times with windows of different lengths.

--- src/main/scala/Day09.scala
+++ src/main/scala/Day09.scala
@@ -1,5 +1,7 @@
 package com.fivebytestudios.wildfreddy
 
+import scala.annotation.tailrec
+
 object Day09 {
   def findInvalid(input: List[Long], preambleLength: Int): Option[Long] =
     input
@@ -16,13 +18,30 @@ object Day09 {
     findInvalid(input.map(_.toLong), preambleLength)
   }
 
+  def takeUntilLimit(input: List[Long], limit: Long): List[Long] = {
+    @tailrec
+    def takeUntilLimitAcc(prefix: List[Long], remainder: List[Long], sum: Long): List[Long] = {
+      val newSum = remainder.head + sum
+      if (newSum <= limit)
+        takeUntilLimitAcc(remainder.head :: prefix, remainder.tail, newSum)
+      else
+        prefix
+    }
+    takeUntilLimitAcc(List.empty, input, 0).reverse
+  }
+
+  def findPrefixWithSum(input: List[Long], sum: Long): Option[List[Long]] = {
+    Some(takeUntilLimit(input, sum))
+      .filter(prefix => prefix.sum == sum && prefix.size >= 2)
+  }
+
   def part2(input: List[String], preambleLength: Int): Option[Long] = {
     val numbers = input.map(_.toLong)
     findInvalid(numbers, preambleLength).flatMap(invalid =>
-      (2 until numbers.length)
-        .flatMap(numbers.sliding)
-        .find(_.sum == invalid)
-        .map(contiguous => contiguous.min + contiguous.max)
+      numbers
+        .tails
+        .map(findPrefixWithSum(_, invalid))
+        .collectFirst{ case Some(contiguous) => contiguous.min + contiguous.max }
     )
   }
 }
