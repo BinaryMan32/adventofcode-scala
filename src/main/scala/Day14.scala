9066c0c0ed6c6bcbf5aa250a530dbf358fbbec61
Fred Fettinger <fettinge@gmail.com>
2020-12-15 02:47:04 -0600

    Day14 Solution

--- src/main/scala/Day14.scala
+++ src/main/scala/Day14.scala
@@ -1,11 +1,79 @@
 package com.fivebytestudios.wildfreddy
 
+import scala.collection.immutable.BitSet
+
 object Day14 {
+  case class Mask(ones: Long = 0, zeros: Long = 0, xs: Long = 0) {
+    def xCombinations: Iterator[Long] =
+      BitSet.fromBitMask(Array(xs))
+        .subsets
+        .map(_.foldLeft(0L)((acc, n) => acc | (1L << n)))
+
+    def apply(value: Long): Long = (value & ~zeros) | ones
+  }
+
+  sealed trait Instruction
+
+  case class SetMask(mask: Mask) extends Instruction
+  object SetMask {
+    val regex = raw"mask = ([01X]+)".r
+    def toMask(value: String, keep: Char): Long =
+      BigInt(value.map(x => if (x == keep) '1' else '0'), 2).toLong
+    def create(mask: String): SetMask = SetMask(
+      mask = Mask(
+        ones = toMask(mask, '1'),
+        zeros = toMask(mask, '0'),
+        xs = toMask(mask, 'X')
+      )
+    )
+  }
+
+  case class SetMem(offset: Long, value: Long) extends Instruction
+  object SetMem {
+    val regex = raw"mem\[([0-9]+)\] = ([0-9]+)".r
+    def create(offset: String, value: String): SetMem = SetMem(
+      offset = offset.toLong,
+      value = value.toLong
+    )
+  }
+
+  object Instruction {
+    def apply(line: String): Instruction = line match {
+      case SetMask.regex(mask) => SetMask.create(mask)
+      case SetMem.regex(offset, value) => SetMem.create(offset, value)
+    }
+  }
+
+  case class State(mask: Mask = Mask(), memory: Map[Long, Long] = Map.empty) {
+    def runV1(instruction: Instruction): State = instruction match {
+      case SetMask(mask) => copy(mask = mask)
+      case SetMem(offset, value) => copy(memory = memory.updated(offset, mask(value)))
+    }
+    def runV2(instruction: Instruction): State = instruction match {
+      case SetMask(mask) => copy(mask = mask)
+      case SetMem(offset, value) => {
+        val baseOffset = (offset | mask.ones) & ~mask.xs
+        val offsets = mask.xCombinations.map(baseOffset | _)
+        copy(memory = memory ++ offsets.map(_ -> value))
+      }
+    }
+  }
+
   def part1(input: List[String]): Long = {
-    0
+    input
+      .map(Instruction(_))
+      .foldLeft(State())((state, instruction) => state.runV1(instruction))
+      .memory
+      .values
+      .sum
   }
 
   def part2(input: List[String]): Long = {
-    0
+    input
+      .map(Instruction(_))
+      .foldLeft(State())((state, instruction) => state.runV2(instruction))
+      .memory
+      .values
+      .sum
   }
 }
--- src/test/scala/Day14Test.scala
+++ src/test/scala/Day14Test.scala
@@ -2,6 +2,7 @@ package com.fivebytestudios.wildfreddy
 
 import helpers.ResourceHelpers
 
+import com.fivebytestudios.wildfreddy.Day14.Mask
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
@@ -11,13 +12,31 @@ class Day14Test extends AnyWordSpec with Matchers with ResourceHelpers {
   private val example2Data = getResourceLines("example2")
   private val inputData = getResourceLines("input")
 
+  "Mask.xCombinations" should {
+    "return only 0" in {
+      Mask(0, 0, 0).xCombinations.toSet should contain theSameElementsAs Set(0)
+    }
+
+    "return 2 combinations" in {
+      Mask(0, 0, 1).xCombinations.toSet should contain theSameElementsAs Set(0, 1)
+    }
+
+    "return 4 combinations" in {
+      Mask(0, 0, 5).xCombinations.toSet should contain theSameElementsAs Set(0, 1, 4, 5)
+    }
+
+    "return biggest combination" in {
+      Mask(0, 0, 1L << 35).xCombinations.toSet should contain theSameElementsAs Set(0, 1L << 35)
+    }
+  }
+
   "part1" should {
     "pass example" in {
       Day14.part1(exampleData) shouldBe 165
     }
 
     "produce result" in {
-      Day14.part1(inputData) shouldBe 0
+      Day14.part1(inputData) shouldBe 9967721333886L
     }
   }
 
@@ -27,7 +46,7 @@ class Day14Test extends AnyWordSpec with Matchers with ResourceHelpers {
     }
 
     "produce result" in {
-      Day14.part2(inputData) shouldBe 0
+      Day14.part2(inputData) shouldBe 4355897790573L
     }
   }
 }
