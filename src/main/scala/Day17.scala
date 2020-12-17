e54f86a4da06a5e3e303a0734468c478a72ed069
Fred Fettinger <fettinge@gmail.com>
2020-12-17 03:01:45 -0600

    Day17 Solution

--- src/main/scala/Day17.scala
+++ src/main/scala/Day17.scala
@@ -1,11 +1,72 @@
 package com.fivebytestudios.wildfreddy
 
+import scala.annotation.tailrec
+
 object Day17 {
+  case class Coordinate(values: Vector[Int]) {
+    import Coordinate._
+    def neighbors: Set[Coordinate] =
+      neighborVectors(values)
+        .filterNot(_ == values)
+        .map(Coordinate(_))
+  }
+
+  object Coordinate {
+    @tailrec
+    def neighborVectors(
+      coordinate: Vector[Int],
+      accumulate: Set[Vector[Int]] = Set(Vector.empty)
+    ): Set[Vector[Int]] =
+      coordinate match {
+        case head +: tail => neighborVectors(
+          tail,
+          accumulate.flatMap(acc =>
+            (head - 1 to head + 1).map(acc :+ _)
+          )
+        )
+        case _ => accumulate
+      }
+  }
+
+  case class State(cubes: Set[Coordinate]) {
+    def next: State = {
+      val newCubes = cubes
+        .toList
+        .flatMap(_.neighbors)
+        .groupMapReduce(identity)(_ => 1)(_ + _)
+        .filter { case (coordinate, neighbors) =>
+          neighbors == 3 || neighbors == 2 && cubes(coordinate)
+        }.keySet
+      State(newCubes)
+    }
+  }
+
+  def initialState(input: List[String], additionalDimensions: Int) : State = {
+    val cubes = for {
+      (row, y) <- input.zipWithIndex
+      (value, x) <- row.zipWithIndex if value == '#'
+    } yield Vector(x, y)
+    State(
+      cubes.map(c => Coordinate(c ++ Vector.fill(additionalDimensions)(0)))
+        .toSet
+    )
+  }
+
+  def simulate(state: State, cycles: Int): State =
+    Iterator
+      .iterate(state)(_.next)
+      .drop(6)
+      .next
+
   def part1(input: List[String]): Long = {
-    0
+    simulate(initialState(input, additionalDimensions = 1), cycles=6)
+      .cubes
+      .size
   }
 
   def part2(input: List[String]): Long = {
-    0
+    simulate(initialState(input, additionalDimensions = 2), cycles=6)
+      .cubes
+      .size
   }
 }
--- src/test/scala/Day17Test.scala
+++ src/test/scala/Day17Test.scala
@@ -1,5 +1,6 @@
 package com.fivebytestudios.wildfreddy
 
+import Day17.Coordinate
 import helpers.ResourceHelpers
 
 import org.scalatest.matchers.should.Matchers
@@ -10,13 +11,35 @@ class Day17Test extends AnyWordSpec with Matchers with ResourceHelpers {
   private val exampleData = getResourceLines("example")
   private val inputData = getResourceLines("input")
 
+  "Coordinate.neighbors" should {
+    "work in 1 dimension" in {
+      Coordinate(Vector(3)).neighbors should contain theSameElementsAs Set(
+        Coordinate(Vector(2)),
+        Coordinate(Vector(4))
+      )
+    }
+
+    "work in 2 dimensions" in {
+      Coordinate(Vector(1,4)).neighbors should contain theSameElementsAs Set(
+        Coordinate(Vector(0, 3)),
+        Coordinate(Vector(1, 3)),
+        Coordinate(Vector(2, 3)),
+        Coordinate(Vector(0, 4)),
+        Coordinate(Vector(2, 4)),
+        Coordinate(Vector(0, 5)),
+        Coordinate(Vector(1, 5)),
+        Coordinate(Vector(2, 5))
+      )
+    }
+  }
+
   "part1" should {
     "pass example" in {
       Day17.part1(exampleData) shouldBe 112
     }
 
     "produce result" in {
-      Day17.part1(inputData) shouldBe 0
+      Day17.part1(inputData) shouldBe 218
     }
   }
 
@@ -26,7 +49,7 @@ class Day17Test extends AnyWordSpec with Matchers with ResourceHelpers {
     }
 
     "produce result" in {
-      Day17.part2(inputData) shouldBe 0
+      Day17.part2(inputData) shouldBe 1908
     }
   }
 }
