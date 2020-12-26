d547e1c4c5a5e0c64b5c4a78b7c2b8d52e8219bb
Fred Fettinger <fettinge@gmail.com>
2020-12-25 23:52:44 -0600

    Day22 Solution

--- src/main/scala/Day22.scala
+++ src/main/scala/Day22.scala
@@ -1,11 +1,86 @@
 package com.fivebytestudios.wildfreddy
 
 object Day22 {
+  def parseDecks(input: List[String]): List[Vector[Int]] = {
+    Utilities.groupLines(input).map(_.tail.map(_.toInt).toVector)
+  }
+  case class State(decks: List[Vector[Int]]) {
+    def nextRound: State = {
+      State(decks = if (decks(0).head > decks(1).head)
+        List(decks(0).tail ++ Vector(decks(0).head, decks(1).head), decks(1).tail)
+      else
+        List(decks(0).tail, decks(1).tail ++ Vector(decks(1).head, decks(0).head))
+      )
+    }
+    def hasEnded: Boolean = decks.exists(_.isEmpty)
+    def score: Long =
+      decks.map(deck =>
+        deck.reverse.zipWithIndex.map{ case (card, index) =>
+          card.toLong * (index + 1)
+        }.sum
+      ).max
+  }
   def part1(input: List[String]): Long = {
-    0
+    Iterator.iterate(State(parseDecks(input)))(_.nextRound)
+      .find(_.hasEnded)
+      .map(_.score)
+      .getOrElse(0)
   }
 
+  case class PlayerState(deck: Vector[Int]) {
+    def top: Int = deck.head
+    def canPlayRecursive: Boolean = deck.tail.size >= deck.head
+    def recurse: PlayerState = PlayerState(deck = deck.tail.take(deck.head))
+    def next(bottom: Vector[Int]): PlayerState = PlayerState(deck = deck.tail ++ bottom)
+    def score: Long =
+      deck.reverse.zipWithIndex.map{ case (card, index) =>
+        card.toLong * (index + 1)
+      }.sum
+  }
+  case class GameOutcome(winIndex: Int, winState: PlayerState)
+  case class RecursiveCombat(players: List[PlayerState], history: Set[List[PlayerState]]) {
+    def gameWinner: Option[Int] =
+      if (history.contains(players))
+        Some(0)
+      else
+        players
+          .zipWithIndex
+          .collectFirst{ case (player, index) if player.deck.isEmpty => index ^ 1 }
+    def gameOutcome: Option[GameOutcome] = gameWinner.map(winner =>
+      GameOutcome(winner, players(winner))
+    )
+    private def roundWinner: Int =
+      if (players.forall(_.canPlayRecursive))
+        playRecursiveCombat(players.map(_.recurse)).winIndex
+      else if (players(0).top > players(1).top)
+        0
+      else
+        1
+    def nextRound: RecursiveCombat = {
+      val bottomCards: List[Vector[Int]] = if (roundWinner == 0)
+        List(Vector(players(0).top, players(1).top), Vector.empty)
+      else
+        List(Vector.empty, Vector(players(1).top, players(0).top))
+      RecursiveCombat(
+        players = (players zip bottomCards).map{case (player, bottom) => player.next(bottom)},
+        history = history + players
+      )
+    }
+  }
+
+  def playRecursiveCombat(players: List[PlayerState]): GameOutcome = {
+    Iterator.iterate(RecursiveCombat(players, Set.empty))(_.nextRound)
+      .find(_.gameWinner.isDefined)
+      .flatMap(_.gameOutcome)
+      .get
+  }
+
+  def playRecursiveCombatInput(input: List[String]): GameOutcome =
+    playRecursiveCombat(parseDecks(input).map(PlayerState))
+
   def part2(input: List[String]): Long = {
-    0
+    playRecursiveCombatInput(input)
+      .winState
+      .score
   }
 }
--- src/test/scala/Day22Test.scala
+++ src/test/scala/Day22Test.scala
@@ -11,22 +11,40 @@ class Day22Test extends AnyWordSpec with Matchers with ResourceHelpers {
   private val inputData = getResourceLines("input")
 
   "part1" should {
+    "parse decks" in {
+      Day22.parseDecks(exampleData) shouldBe List(
+        Vector(9, 2, 6, 3, 1),
+        Vector(5, 8, 4, 7, 10)
+      )
+    }
     "pass example" in {
       Day22.part1(exampleData) shouldBe 306
     }
 
     "produce result" in {
-      Day22.part1(inputData) shouldBe 0
+      Day22.part1(inputData) shouldBe 32489
     }
   }
 
   "part2" should {
+    "player 1 wins due to detecting recursion" in {
+      val input = """Player 1:
+                    |43
+                    |19
+                    |
+                    |Player 2:
+                    |2
+                    |29
+                    |14
+                    |""".stripMargin.split("\n").toList
+      Day22.playRecursiveCombatInput(input).winIndex shouldBe 0
+    }
     "pass example" in {
       Day22.part2(exampleData) shouldBe 291
     }
 
     "produce result" in {
-      Day22.part2(inputData) shouldBe 0
+      Day22.part2(inputData) shouldBe 35676
     }
   }
 }
