eb160ed78105b1e6308d3d073330accace61222f
Fred Fettinger <fettinge@gmail.com>
2020-12-25 19:33:04 -0600

    Day21 Template

---
+++ src/test/scala/Day21Test.scala
@@ -0,0 +1,38 @@
+package com.fivebytestudios.wildfreddy
+
+import helpers.ResourceHelpers
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class Day21Test extends AnyWordSpec with Matchers with ResourceHelpers {
+  override val resourcePath = "day21"
+  private val exampleData = getResourceLines("example")
+  private val inputData = getResourceLines("input")
+
+  "part1" should {
+    "parse food" in {
+      Day21.parseFood(exampleData.head) shouldBe Day21.Food(
+        ingredients = Set("mxmxvkd", "kfcds", "sqjhc", "nhms"),
+        allergens = Set("dairy", "fish")
+      )
+    }
+    "pass example" in {
+      Day21.part1(exampleData) shouldBe 5
+    }
+
+    "produce result" in {
+      Day21.part1(inputData) shouldBe 0
+    }
+  }
+
+  "part2" should {
+    "pass example" in {
+      Day21.part2(exampleData) shouldBe "mxmxvkd,sqjhc,fvjkl"
+    }
+
+    "produce result" in {
+      Day21.part2(inputData) shouldBe ""
+    }
+  }
+}
