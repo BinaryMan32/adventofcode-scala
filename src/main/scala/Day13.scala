393746d2b5620a1772a0bced8f5c5930a7550a63
Fred Fettinger <fettinge@gmail.com>
2020-12-26 22:17:19 -0600

    Day13 Part2 Solution

--- src/main/scala/Day13.scala
+++ src/main/scala/Day13.scala
@@ -1,5 +1,7 @@
 package com.fivebytestudios.wildfreddy
 
+import scala.annotation.tailrec
+
 object Day13 {
   def part1(input: List[String]): Long = {
     val startTimestamp = input.head.toInt
@@ -10,7 +12,40 @@ object Day13 {
     id * wait
   }
 
+  case class Constraint(offset: Long, period: Long) {
+    def possibilities: Iterator[Long] =
+      Iterator.iterate(offset)(_ + period)
+    def satisfies(test: Long): Boolean =
+      (test - offset) % period == 0
+  }
+
+  def reduceConstraints(a: Constraint, b: Constraint): Constraint =
+    if (a.period > b.period)
+      reduceConstraints(b, a)
+    else
+      Constraint(
+        offset = b.possibilities.find(a.satisfies).get,
+        period = a.period * b.period
+      )
+
+  def parseConstraints(line: String): Iterable[Constraint] =
+    line
+      .split(",")
+      .zipWithIndex
+      .filterNot(_._1 == "x")
+      .map{ case(bus, index) =>
+        Constraint(
+          offset = bus.toInt - index,
+          period = bus.toInt
+        )
+      }
+
+  def findConstraintTime(line: String): Long =
+    parseConstraints(line)
+      .reduce(reduceConstraints)
+      .offset
+
   def part2(input: List[String]): Long = {
-    0
+    findConstraintTime(input(1))
   }
 }
--- src/test/scala/Day13Test.scala
+++ src/test/scala/Day13Test.scala
@@ -20,13 +20,27 @@ class Day13Test extends AnyWordSpec with Matchers with ResourceHelpers {
     }
   }
 
+  "reduceConstraints" should {
+    import Day13.{reduceConstraints, Constraint}
+
+    "solve small examples" in {
+      reduceConstraints(Constraint(0, 2), Constraint(1, 3)) shouldBe Constraint(4, 6)
+      reduceConstraints(Constraint(1, 3), Constraint(0, 2)) shouldBe Constraint(4, 6)
+    }
+  }
+
   "part2" should {
     "pass example" in {
-      Day13.part2(exampleData) shouldBe 0
+      Day13.findConstraintTime("17,x,13,19") shouldBe 3417
+      Day13.findConstraintTime("67,7,59,61") shouldBe 754018
+      Day13.findConstraintTime("67,x,7,59,61") shouldBe 779210
+      Day13.findConstraintTime("67,7,x,59,61") shouldBe 1261476
+      Day13.findConstraintTime("1789,37,47,1889") shouldBe 1202161486
+      Day13.part2(exampleData) shouldBe 1068781
     }
 
     "produce result" in {
-      Day13.part2(inputData) shouldBe 0
+      Day13.part2(inputData) shouldBe 800177252346225L
     }
   }
 }
