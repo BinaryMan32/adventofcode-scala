b45bcc8445a7f54c5c3fe4f5f446ce870afefb02
Fred Fettinger <fettinge@gmail.com>
2020-12-26 18:27:16 -0600

    Day24 Solution

--- src/main/scala/Day24.scala
+++ src/main/scala/Day24.scala
@@ -1,11 +1,102 @@
 package com.fivebytestudios.wildfreddy
 
+import scala.annotation.tailrec
+
 object Day24 {
+  object TileNeighbor extends Enumeration {
+    type TileNeighbor = Value
+    val East, SouthEast, SouthWest, West, NorthWest, NorthEast = Value
+    def parseLine(line: String): Seq[TileNeighbor] = {
+      @tailrec
+      def parseLineAux(parsed: List[TileNeighbor], line: String):
+        Seq[TileNeighbor] = line.headOption match {
+        case Some('n') => line(1) match {
+          case 'e' => parseLineAux(NorthEast :: parsed, line.drop(2))
+          case 'w' => parseLineAux(NorthWest :: parsed, line.drop(2))
+        }
+        case Some('e') => parseLineAux(East :: parsed, line.tail)
+        case Some('s') => line(1) match {
+          case 'e' => parseLineAux(SouthEast :: parsed, line.drop(2))
+          case 'w' => parseLineAux(SouthWest :: parsed, line.drop(2))
+        }
+        case Some('w') => parseLineAux(West :: parsed, line.tail)
+        case None => parsed
+      }
+      parseLineAux(List.empty, line).reverse
+    }
+  }
+  import TileNeighbor._
+
+  /**
+   * Represents a tile addressing scheme with neighbor transitions.
+   *
+   * This models movement based on the idea that advancing diagonally should
+   * move half as much in X as in Y. As long as all movement begins at ORIGIN,
+   * coordinates are limited to the following:
+   * 1. if y is even, x must be even
+   * 2. if y is odd, x must be odd
+   */
+  case class TileAddress(x: Int, y: Int) {
+    def move(neighbor: TileNeighbor): TileAddress = neighbor match {
+      case NorthWest => TileAddress(x - 1, y + 1)
+      case NorthEast => TileAddress(x + 1, y + 1)
+      case West => TileAddress(x - 2, y)
+      case East => TileAddress(x + 2, y)
+      case SouthWest => TileAddress(x - 1, y - 1)
+      case SouthEast => TileAddress(x + 1, y - 1)
+    }
+    def adjacent: Iterable[TileAddress] = TileNeighbor.values.toSeq.map(move)
+    def atPathEnd(neighbors: Iterable[TileNeighbor]): TileAddress =
+      neighbors.foldRight(this)((neighbor, address) => address.move(neighbor))
+  }
+
+  object TileAddress {
+    private val ORIGIN = TileAddress(0, 0)
+    def resolvePath(neighbors: Iterable[TileNeighbor]): TileAddress =
+      ORIGIN.atPathEnd(neighbors)
+  }
+
+  case class Floor(blackTiles: Set[TileAddress] = Set.empty) {
+    def flipTile(address: TileAddress): Floor =
+      copy(blackTiles =
+        if (blackTiles.contains(address))
+          blackTiles - address
+        else
+          blackTiles + address
+      )
+
+    def numBlackTiles: Int = blackTiles.size
+
+    def nextDay: Floor = {
+      val numBlackAdjacent = blackTiles
+        .toSeq
+        .flatMap(_.adjacent)
+        .groupMapReduce(identity)(_ => 1)(_ + _)
+      val blackTilesNoAdjacent = blackTiles -- numBlackAdjacent.keySet
+      val blackTilesMoreThan2Adj = blackTiles & numBlackAdjacent.filter(_._2 > 2).keySet
+      val flipToWhite = blackTilesNoAdjacent ++ blackTilesMoreThan2Adj
+      val whiteTilesWith2Adj = numBlackAdjacent.filter(_._2 == 2).keySet -- blackTiles
+      val flipToBlack = whiteTilesWith2Adj
+      copy(blackTiles = blackTiles -- flipToWhite ++ flipToBlack)
+    }
+  }
+
+  object Floor {
+    def fromInput(input: List[String]): Floor =
+      input.map(TileNeighbor.parseLine)
+        .foldRight(Floor())((path, floor) =>
+          floor.flipTile(TileAddress.resolvePath(path))
+        )
+  }
+
   def part1(input: List[String]): Long = {
-    0
+    Floor.fromInput(input).numBlackTiles
   }
 
   def part2(input: List[String]): Long = {
-    0
+    Iterator.iterate(Floor.fromInput(input))(_.nextDay)
+      .drop(100)
+      .next
+      .numBlackTiles
   }
 }
--- src/test/scala/Day24Test.scala
+++ src/test/scala/Day24Test.scala
@@ -2,6 +2,7 @@ package com.fivebytestudios.wildfreddy
 
 import helpers.ResourceHelpers
 
+import com.fivebytestudios.wildfreddy.Day24.TileNeighbor
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
@@ -10,13 +11,58 @@ class Day24Test extends AnyWordSpec with Matchers with ResourceHelpers {
   private val exampleData = getResourceLines("example")
   private val inputData = getResourceLines("input")
 
+  "TileNeighbor" should {
+    import Day24.TileNeighbor._
+    "parse correctly" in {
+      parseLine("esew") shouldBe List(East, SouthEast, West)
+      parseLine("nwwswee") shouldBe List(NorthWest, West, SouthWest, East, East)
+      parseLine("neee") shouldBe List(NorthEast, East, East)
+    }
+  }
+
+  "TileAddress" should {
+    import Day24.TileAddress
+    import Day24.TileNeighbor._
+    "return to itself" in {
+      val origin = TileAddress(0, 0)
+      origin.move(NorthWest).move(SouthEast) shouldBe origin
+      origin.move(NorthEast).move(SouthWest) shouldBe origin
+      origin.move(West).move(East) shouldBe origin
+      origin.move(East).move(West) shouldBe origin
+      origin.move(SouthWest).move(NorthEast) shouldBe origin
+      origin.move(SouthEast).move(NorthWest) shouldBe origin
+    }
+
+    def resolvePath(path: String): TileAddress =
+      TileAddress.resolvePath(TileNeighbor.parseLine(path))
+
+    "make triangles" in {
+      resolvePath("nenesese") shouldBe resolvePath("ee")
+      resolvePath("sesenene") shouldBe resolvePath("ee")
+      resolvePath("nwnwswsw") shouldBe resolvePath("ww")
+      resolvePath("swswnwnw") shouldBe resolvePath("ww")
+    }
+
+    "make trapezoids" in {
+      resolvePath("neneee") shouldBe resolvePath("eenene")
+      resolvePath("seseee") shouldBe resolvePath("eesese")
+      resolvePath("nwnwww") shouldBe resolvePath("wwnwnw")
+      resolvePath("swswww") shouldBe resolvePath("wwswsw")
+    }
+
+    "from example" in {
+      resolvePath("esew") shouldBe resolvePath("se")
+      resolvePath("nwwswee") shouldBe resolvePath("")
+    }
+  }
+
   "part1" should {
     "pass example" in {
       Day24.part1(exampleData) shouldBe 10
     }
 
     "produce result" in {
-      Day24.part1(inputData) shouldBe 0
+      Day24.part1(inputData) shouldBe 375
     }
   }
 
@@ -26,7 +72,7 @@ class Day24Test extends AnyWordSpec with Matchers with ResourceHelpers {
     }
 
     "produce result" in {
-      Day24.part2(inputData) shouldBe 0
+      Day24.part2(inputData) shouldBe 3937
     }
   }
 }
